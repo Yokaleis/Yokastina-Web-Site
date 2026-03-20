bd8d5ac2a157dcd32731f1f7dd75ec24da4bb31f
Yokaleis <yokaleis@gmail.com.com>
2026-03-20 15:57:53 -0400

    chore: Cambios en footer made in Venezuela

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -20,7 +20,7 @@ export function Footer() {
             </div>
         </div>
         <div className="grid grid-cols-1 text-center">
-                <p>Todos los derechos reservados</p>
+                <p className="text-xs font-semibold text-gray-500">Made in Venezuela 💛💙❤️ | 2026</p>
             </div>
 
     </div>
